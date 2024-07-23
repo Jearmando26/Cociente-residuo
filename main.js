b496a315aeb12e2015935441963b2a0fc8cab070
Jesus Armando Guerrero Montilla <armand26112005@gmail.com>
2024-07-23 11:19:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// variables
+
+let n1, n2, cociente, residuo;
+
+// Datos
+
+n1 = Number(prompt('Ingrese un numero'));
+n2 = Number(prompt('Ingrese un numero'));
+
+// Proceso
+
+cociente = n1 / n2;
+residuo = n1 % n2;
+
+// Salid de datos
+
+document.write('Cociente:' + cociente + '<br>');
+document.write('Residuo:' + residuo + '<br>');
